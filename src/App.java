b56caf7714e47651bc0f749f6ff0c0c3153ba3f3
patriciomashinkiash <morapatricio152@gmail.com>
2024-09-27 12:51:03 -0500

    p

--- src/App.java
+++ src/App.java
@@ -2,5 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Metodo de ordenamiento seleccion");
         MetodosOrdenamiento mD = new MetodosOrdenamiento();
+
+        int[] arreglo = {10,5,8,2,0};
+        mD.sortByseleccion(arreglo, true);
+        mD.printArreglo(arreglo);
+        System.out.println();
+
+        mD.sortByseleccion(arreglo, false);
+        mD.printArreglo(arreglo);
+            
     }
 }
--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -1,22 +1,30 @@
-class MetodosOrdenamiento{
+class MetodosOrdenamiento {
 
-    public int[] sortByseleccion(int[] arreglo){
+    public int[] sortByseleccion(int[] arreglo, boolean asen) {
         int tam = arreglo.length;
 
-        //Recorrer el arreglo
-        for(int i=0; i<tam;i++){
+        // Recorrer el arreglo
+        for (int i = 0; i < tam; i++) {
             int indice = i;
-            for(int j=i+1;i<tam;j++){
-                if(arreglo[j] > arreglo[indice]){
+            for (int j = i + 1; j < tam; j++) {
+                if (asen
+                        ? arreglo[j] < arreglo[indice]
+                        : arreglo[j] > arreglo[indice]) {
                     indice = j;
                 }
             }
-            //Intercambio del indice encontrado por el primero
+            // Intercambio del indice encontrado por el primero+gut 
             int aux = arreglo[indice];
             arreglo[indice] = arreglo[i];
             arreglo[i] = aux;
         }
         return arreglo;
+    }
+
+    public void printArreglo(int[] arreglo) {
+        for (int elem : arreglo) {
+            System.out.print(elem + " ");
+        }
     }
 
 }
